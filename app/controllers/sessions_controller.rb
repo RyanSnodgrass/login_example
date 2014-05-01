d02e18392e940f54133cc40c37cab1d58a2577f6
RyanSnodgrass <res0428@yahoo.com>
2014-05-01 10:10:50 -0400

    update sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,1 +1,15 @@
-sessions_controller.rb
+class SessionsController < ApplicationController
+	def new
+	end
+
+	def create
+		user = User.authenticate(params[:email],params[:password])
+		if user
+			session[:user_id] = user.user.id
+			redirect_to , notice: "logged in"
+		else
+			flash.now.alert = "invalid email or pw"
+			render :new
+		end
+	end
+end
