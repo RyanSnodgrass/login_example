257aaad6684483bbc0a756a1cd3d57769b8e7a67
RyanSnodgrass <res0428@yahoo.com>
2014-05-01 11:15:00 -0400

    if we used has_secure_pasword we dont even need other stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,23 @@
-require 'bcrypt'
+# require 'bcrypt'
 class User < ActiveRecord::Base
-	attr_accessor :password, :password_confirmation
-	before_save :encrypt_password
+	has_secure_password
+	#attr_accessor :password, :password_confirmation
+	#before_save :encrypt_password
 
 	validates_presence_of :email
 	validates_uniqueness_of :email
-	validates_confirmation_of :password
+	#validates_confirmation_of :password
 
-	def encrypt_password
-		self.password_salt = BCrypt::Engine.generate_salt
-		self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-	end
-	def self.authenticate(email, password)
-		user = User.find_by_email(email) # active record methods
-		if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-			user
-		else
-			nil
-		end
-	end
-end
+# 	def encrypt_password
+# 		self.password_salt = BCrypt::Engine.generate_salt
+# 		self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+# 	end
+# 	def self.authenticate(email, password)
+# 		user = User.find_by_email(email) # active record methods
+# 		if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+# 			user
+# 		else
+# 			nil
+# 		end
+# 	end
+# end
