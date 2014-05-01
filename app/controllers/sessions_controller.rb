28de70cf7a3fc1d2bba7b4196a24efd92e5375e0
RyanSnodgrass <res0428@yahoo.com>
2014-05-01 10:31:48 -0400

    fixed errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.authenticate(params[:email],params[:password])
+		user = User.authenticate(params[:email], params[:password])
 		if user
-			session[:user_id] = user.user.id
+			session[:user_id] = user.id
 			redirect_to new_user_path, notice: "logged in"
 		else
 			flash.now.alert = "invalid email or pw"
