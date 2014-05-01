e1b49caf040c978f976f4b53e45517e1d2d19dab
RyanSnodgrass <res0428@yahoo.com>
2014-05-01 11:23:29 -0400

    ran rails migration to add a password_digest column to users tbale

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501135520) do
+ActiveRecord::Schema.define(version: 20140501152219) do
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140501135520) do
     t.string   "password_hash"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
