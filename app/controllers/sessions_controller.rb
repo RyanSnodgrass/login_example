41bf4828f8069cc1189ed5d8e274a1dc560f8c5c
RyanSnodgrass <res0428@yahoo.com>
2014-05-01 10:37:02 -0400

    added notes to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,10 @@ class SessionsController < ApplicationController
 	def create
 		user = User.authenticate(params[:email], params[:password])
 		if user
-			session[:user_id] = user.id
+			#session starts as an empty hash
+			#basically creating a hash {session: {user_id: user.id}}
+			session[:user_id] = user.id #session is a rails variable for cookies stored in browser
+				# session[:user_id] is arbitrary. if i want i can call it session[:current_user_id]
 			redirect_to new_user_path, notice: "logged in"
 		else
 			flash.now.alert = "invalid email or pw"
