1ab698c6c5960a67c1b6d3972d5cd762343f78db
RyanSnodgrass <res0428@yahoo.com>
2014-05-01 09:43:12 -0400

    added all stuff up to now

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+	def new
+		@new_user = User.new
+	end
+	def create
+		@new_user = User.new(user_params)
+		if @new_user.save
+			redirect_to new_user_path, notice: "You Signed up!"
+		else
+			render :new, notice: "You failed."
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit!
+	end
+end
