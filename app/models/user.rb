77a0d28ed2a6b3259889284dc976f58c9b7517f5
RyanSnodgrass <res0428@yahoo.com>
2014-05-01 09:44:23 -0400

    user model added _of

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	attr_accessor :password, :password_confirmation
+
+	validates_presence_of :email
+	validates_uniqueness_of :email
+	validates_confirmation_of :password
 end
