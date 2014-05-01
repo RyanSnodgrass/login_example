68b03ddc2c2fd9aafa2b282972d1e594cafa3f91
RyanSnodgrass <res0428@yahoo.com>
2014-05-01 11:30:37 -0400

    added filter to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :ensure_logged_in
+
   helper_method :current_user
 
   def current_user
   	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def ensure_logged_in
+  	if current_user.nil?
+  		redirect_to login_path
+  	end
   end
 end
